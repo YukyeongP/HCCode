a9f4dee4d2490f1c7224f80d72519f7dec93a2b1
YukyeongP <75259098+YukyeongP@users.noreply.github.com>
2021-12-24 09:18:58 +0900

    Add files via upload
    C#Code

---
+++ HCcsCode/Fourbox.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+// Fourbox problem
+
+namespace HCcsCode
+{
+    public class Fourbox
+    {
+        private string boxPstr;
+
+        // constructor
+        public Fourbox(string boxPstr)
+        {
+            this.boxPstr = boxPstr;
+        }
+
+        public int Area()
+        {
+            var boxP = toBoxes(boxPstr); //
+            var pt = new List<Tuple<int, int>>();
+
+            for (var i = 0; i < 4; i++)
+                for (int j = boxP[i, 0] + 1; j < boxP[i, 2] + 1; j++)
+                    for (int k = boxP[i, 1] + 1; k < boxP[i, 3] + 1; k++)
+                        pt.Add(new Tuple<int, int>(j, k));
+
+            return pt.Distinct().Count();
+        }
+
+        private int[,] toBoxes(string s)
+        {
+            // put Exception Handling
+            var exp = new int[,] { {0}, {0} };
+            try
+            {
+                var strboxP = s.Split(' ')
+                           .Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                var intBoxP = Array.ConvertAll(strboxP, int.Parse);
+                var boxes = new int[4, 4];
+
+                for (int i = 0; i < boxes.GetLength(0); i++)
+                    for (int j = 0; j < boxes.GetLength(1); j++)
+                        boxes[i, j] = intBoxP[i * 4 + j];
+
+                return boxes;
+            }
+            catch when (s == null)
+            {
+                return exp;
+            }
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            var b1 = "0 0 200 200    0 0 200 200    0 0 200 200    0 0 200 200";
+            var b2 = "1 2 4 4    2 3 5 7    3 1 6 5    7 3 8 6";
+            var b3 = "";
+
+            var box = new Fourbox(b1);
+            //box.ToBoxes();
+            //box.Area();
+
+            Debug.Assert(box.Area() == 40000);
+            box = new Fourbox(b2);
+            Debug.Assert(box.Area() == 26);
+
+            box = new Fourbox(b3);
+
+        }
+    }
+}
+
+
+
---
+++ HCcsCode/StringAdder.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+// String adder problem
+
+namespace HCcsCode
+{
+    // visibility: public internal private
+    static class StringExtention
+    {
+        public static string Reverse(this string self)
+        {
+            var charArray = self.ToCharArray();
+            for (var i=0; i<self.Length/2; i++)
+            {
+                var tmp = charArray[i];
+                charArray[i] = charArray[self.Length - 1 - i];
+                charArray[self.Length - 1 - i] = tmp;
+            }
+
+            return new string(charArray);
+        }
+
+
+        public static string Reverse2(this string self)
+        {
+            char[] charArray = self.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+
+    public class StringAdder
+    {
+        private string str1;
+        private string str2;
+        private string result;
+
+
+        public StringAdder(string str1, string str2)
+        {
+            this.str1 = str1;
+            this.str2 = str2;
+        }
+
+
+        public string Adder(string str1, string str2)
+        {
+            if (str1.Length < str2.Length)
+            {
+                var temp = str2;
+                str2 = str1;
+                str1 = temp;
+            }
+
+            // var str3 = string.ChooseLongerBetween(str1, str2);
+
+            str2 = str2.PadLeft(totalWidth: str1.Length, paddingChar: '0');
+
+
+            var carry = 0;
+            var res = 0;
+            for (int i = str1.Length - 1; i >= 0; --i)
+            {
+                var sum = (str1[i] - '0') + (str2[i] - '0') + carry;
+                carry = sum / 10;
+    
+                if (carry == 1)
+                    res+=1;
+
+                res = sum % 10;
+                result += res.ToString();
+            }
+
+            return result.Reverse2();
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var str1 = "123";
+                var str2 = "4567";
+
+                var xx = str2.Reverse();
+                var yy = str2.Reverse2();
+                //var strAdder = new StringAdder(str1, str2);
+
+                //Console.WriteLine(strAdder.Adder(str1, str2));
+                //Debug.Assert(str1 + str2 == "4690");
+            }
+        }
+    }
+}
